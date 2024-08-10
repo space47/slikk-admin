a4fda8acb0d09bb105c2e6ec8c2f535f93e2b3a6
parthSlikk <parth@slikk.club>
2024-08-10 14:18:10 +0530

    process is not defined error

--- src/utils/intercepter/globalInterceptorSetup.ts
+++ src/utils/intercepter/globalInterceptorSetup.ts
@@ -1,5 +1,8 @@
 import { setupInterceptorsTo } from "./Interceptors";
 import axios from "axios";
-axios.defaults.baseURL = process.env.BACKEND_URI;
+const BASE_URL = import.meta.env.VITE_BACKEND_URI;
+console.log(BASE_URL);
+
+axios.defaults.baseURL = BASE_URL;
 const axioisInstance = setupInterceptorsTo(axios);
 export default axioisInstance;
