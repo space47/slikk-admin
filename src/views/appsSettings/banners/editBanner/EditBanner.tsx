af0cb32699701e09fb2589ec95dc099593d4150b
bipinSlikk <bipin@slikk.club>
2024-08-19 03:27:50 +0530

    Update EditBanner.tsx

--- src/views/appsSettings/banners/editBanner/EditBanner.tsx
+++ src/views/appsSettings/banners/editBanner/EditBanner.tsx
@@ -165,7 +165,7 @@ const EditBanner = () => {
         files.forEach((file) => {
             formData.append('file', file)
         })
-        formData.append('file_type', 'grn')
+        formData.append('file_type', 'product')
 
         try {
             console.log(formData.get('file'))
