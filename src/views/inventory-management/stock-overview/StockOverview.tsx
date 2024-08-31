0f7d59f181ae4c579e9f5b6a6572fe84e55ee6be
Debashish <debashish@slikk.club>
2024-08-31 14:00:03 +0530

    on 31-08-2024

--- src/views/inventory-management/stock-overview/StockOverview.tsx
+++ src/views/inventory-management/stock-overview/StockOverview.tsx
@@ -127,7 +127,7 @@ const StockOverview = () => {
                 `inventory?p=${page}&page_size=${pageSize}${type}${searchInputType}`,
             )
 
-            if (response.data.data.results.length === 0) {
+            if (response?.data?.data?.results?.length === 0) {
                 searchInputType = `&name=${filter}`
                 setFilterInput(searchInputType)
                 response = await axiosInstance.get(
@@ -135,8 +135,8 @@ const StockOverview = () => {
                 )
             }
 
-            const data = response.data.results
-            const total = response.data.count
+            const data = response.data.data.results
+            const total = response.data.data.count
 
             setData(data)
             setTotalData(total)
