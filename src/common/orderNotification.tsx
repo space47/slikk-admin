a7588d213a0f9304c63b30b18ac263bee0467255
Debashish <debashish@slikk.club>
2024-09-26 17:13:08 +0530

    on 26-09-2024

--- src/common/orderNotification.tsx
+++ src/common/orderNotification.tsx
@@ -2,10 +2,9 @@ import React, { useEffect, useRef } from 'react'
 
 interface SOUNDPROPS {
     shouldPlay: boolean
-    onSoundEnd: () => void
 }
 
-const NotificationSound = ({ shouldPlay, onSoundEnd }: SOUNDPROPS) => {
+const NotificationSound = ({ shouldPlay }: SOUNDPROPS) => {
     const audioRef = useRef<HTMLAudioElement>(null)
 
     useEffect(() => {
@@ -23,19 +22,19 @@ const NotificationSound = ({ shouldPlay, onSoundEnd }: SOUNDPROPS) => {
                             audioRef.current.pause()
                             audioRef.current.currentTime = 0
                         }
-                        onSoundEnd() // Signal that the sound has ended
+                        // Signal that the sound has ended
                     }, 10000) // Adjust this duration if needed
 
                     return () => clearTimeout(timeoutId)
                 } catch (error) {
                     console.error('Error playing sound:', error)
-                    onSoundEnd() // Reset sound if an error occurs
+                    // Reset sound if an error occurs
                 }
             }
         }
 
         playSound()
-    }, [shouldPlay, onSoundEnd])
+    }, [shouldPlay])
 
     return <audio ref={audioRef} src="/notifiactionMusic/order_received_grab.mp3" preload="auto" />
 }
