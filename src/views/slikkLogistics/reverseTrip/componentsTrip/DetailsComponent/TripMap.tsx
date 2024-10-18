2fb5a9da57ec78fa39908001ea97dfd2f17f5ae1
Debashish <debashish@slikk.club>
2024-10-18 23:00:58 +0530

    18-10-2024

--- src/views/slikkLogistics/reverseTrip/componentsTrip/DetailsComponent/TripMap.tsx
+++ src/views/slikkLogistics/reverseTrip/componentsTrip/DetailsComponent/TripMap.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { GoogleMap, MarkerF, LoadScript, Polyline } from '@react-google-maps/api'
+import { GoogleMap, MarkerF, LoadScript, DirectionsRenderer, useJsApiLoader } from '@react-google-maps/api'
 import axiosInstance from '@/utils/intercepter/globalInterceptorSetup'
 import { LogisticTask } from './DetailsCommon'
 
@@ -10,7 +10,49 @@ interface LogisticsMapProps {
 
 const TripMap: React.FC<LogisticsMapProps> = ({ logistic_tasks, trip_id }) => {
     const [logData, setLogData] = useState<LogisticTask[]>([])
-    const [mapref, setMapRef] = useState<google.maps.Map>()
+    const [mapRef, setMapRef] = useState<google.maps.Map>()
+    const [direction, setDirection] = useState<google.maps.DirectionsResult | null>(null)
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: import.meta.env.VITE_MAP_API_KEY,
+        libraries: ['places'],
+    })
+
+    const handleOnLoad = (map: google.maps.Map) => {
+        setMapRef(map)
+
+        if (logData.length > 1) {
+            const DirectionsService = new google.maps.DirectionsService()
+            const origin = new google.maps.LatLng(logData[0].pickup_details.latitude, logData[0].pickup_details.longitude)
+
+            const destination = new google.maps.LatLng(
+                logData[logData.length - 1].drop_details.latitude,
+                logData[logData.length - 1].drop_details.longitude,
+            )
+
+            const waypoints = logData.slice(1, logData.length - 1).map((task) => ({
+                location: new google.maps.LatLng(task.drop_details.latitude, task.drop_details.longitude),
+                stopover: true,
+            }))
+
+            DirectionsService.route(
+                {
+                    origin,
+                    destination,
+                    waypoints,
+                    travelMode: google.maps.TravelMode.DRIVING,
+                },
+                (result, status) => {
+                    if (status === google.maps.DirectionsStatus.OK) {
+                        setDirection(result)
+                    } else {
+                        console.error(`Error fetching directions: ${status}`)
+                    }
+                },
+            )
+        }
+    }
 
     const fetchMainData = async () => {
         try {
@@ -28,13 +70,8 @@ const TripMap: React.FC<LogisticsMapProps> = ({ logistic_tasks, trip_id }) => {
         return () => clearInterval(intervalId)
     }, [trip_id])
 
-    const zoom = 12
-    const mapContainerStyle = {
-        width: '100%',
-        height: '500px',
-    }
-
-    const googleMapsApiKey = import.meta.env.VITE_MAP_API_KEY
+    const zoom = 16
+    const mapContainerStyle = { width: '100%', height: '500px' }
 
     const icons = {
         pickup: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
@@ -42,76 +79,76 @@ const TripMap: React.FC<LogisticsMapProps> = ({ logistic_tasks, trip_id }) => {
         runner: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
     }
 
-    const labels = {
-        pickup: { text: 'Pickup', color: 'white', fontSize: '10px', fontWeight: 'bold' },
-        drop: { text: 'Drop', color: 'white', fontSize: '10px', fontWeight: 'bold' },
-        runner: { text: 'Runner', color: 'white', fontSize: '10px', fontWeight: 'bold' },
-    }
-
-    const handleOnLoad = (map: google.maps.Map) => {
-        setMapRef(map)
-    }
-
     return (
-        <LoadScript googleMapsApiKey={googleMapsApiKey}>
-            <div className="flex flex-col gap-10  w-[80%]">
-                {logData.map((item, index) => (
-                    <div key={index}>
-                        <div className="font-semibold text-xl mb-2">
-                            TaskId: <span className="font-bold">{item.task_id}</span>
-                        </div>
-                        <GoogleMap
-                            onLoad={handleOnLoad}
-                            mapContainerStyle={mapContainerStyle}
-                            zoom={zoom}
-                            center={{
-                                lat: item?.drop_details.latitude,
-                                lng: item?.drop_details.longitude,
-                            }}
-                            options={{
-                                fullscreenControl: true,
-                                keyboardShortcuts: false,
-                                mapTypeControl: false,
-                                streetViewControl: false,
-                                zoomControlOptions: {
-                                    position: 9,
-                                },
-                            }}
-                        >
-                            {/* Pickup Marker */}
+        <div className="flex flex-col gap-10 w-[80%]">
+            {isLoaded && logData.length > 0 ? (
+                <GoogleMap
+                    onLoad={handleOnLoad}
+                    mapContainerStyle={mapContainerStyle}
+                    zoom={zoom}
+                    center={{
+                        lat: logData[0].drop_details.latitude,
+                        lng: logData[0].drop_details.longitude,
+                    }}
+                    options={{
+                        fullscreenControl: true,
+                        keyboardShortcuts: false,
+                        mapTypeControl: false,
+                        streetViewControl: false,
+                        zoomControlOptions: { position: 9 },
+                    }}
+                >
+                    {logData.map((item, index) => (
+                        <React.Fragment key={index}>
                             <MarkerF
                                 position={{
-                                    lat: item?.pickup_details.latitude,
-                                    lng: item?.pickup_details.longitude,
+                                    lat: item.pickup_details.latitude,
+                                    lng: item.pickup_details.longitude,
                                 }}
                                 icon={icons.pickup}
-                                label={labels.pickup}
+                                label={{
+                                    text: `${item?.pickup_details.name}`,
+                                    color: 'white',
+                                    fontSize: '13px',
+                                    fontWeight: 'bold',
+                                }}
                             />
-
-                            {/* Drop Marker */}
                             <MarkerF
                                 position={{
-                                    lat: item?.drop_details.latitude,
-                                    lng: item?.drop_details.longitude,
+                                    lat: item.drop_details.latitude,
+                                    lng: item.drop_details.longitude,
                                 }}
                                 icon={icons.drop}
-                                label={labels.drop}
-                            />
-
-                            {/* Runner Marker */}
-                            <MarkerF
-                                position={{
-                                    lat: item?.runner_latitude,
-                                    lng: item?.runner_longitude,
+                                label={{
+                                    text: `${item?.drop_details.name}`,
+                                    color: 'white',
+                                    fontSize: '13px',
+                                    fontWeight: 'bold',
                                 }}
-                                icon={icons.runner}
-                                label={labels.runner}
                             />
-                        </GoogleMap>
-                    </div>
-                ))}
-            </div>
-        </LoadScript>
+                            {item.runner_latitude && item.runner_longitude && (
+                                <MarkerF
+                                    position={{
+                                        lat: item.runner_latitude,
+                                        lng: item.runner_longitude,
+                                    }}
+                                    icon={icons.runner}
+                                    label={{
+                                        text: `${item?.runner_detail?.name}`,
+                                        color: 'white',
+                                        fontSize: '13px',
+                                        fontWeight: 'bold',
+                                    }}
+                                />
+                            )}
+                        </React.Fragment>
+                    ))}
+                    {direction && <DirectionsRenderer directions={direction} />}
+                </GoogleMap>
+            ) : (
+                <p>Loading map...</p>
+            )}
+        </div>
     )
 }
 
