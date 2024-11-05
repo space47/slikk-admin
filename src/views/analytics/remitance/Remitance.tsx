29135a744a08d63656d415fc72e36c88099382f5
Debashish <debashish@slikk.club>
2024-11-05 17:41:29 +0530

    5-11-2024

--- src/views/analytics/remitance/Remitance.tsx
+++ src/views/analytics/remitance/Remitance.tsx
@@ -132,7 +132,10 @@ const Remitance = () => {
 
     const columns = useMemo(
         () => [
+            { header: 'Invoice Id', accessorKey: 'invoice_id' },
             { header: 'SKU', accessorKey: 'sku' },
+            { header: 'Creation Date', accessorKey: 'create_date' },
+            { header: 'Completed Date', accessorKey: 'complete_date' },
             { header: 'Product Name', accessorKey: 'name' },
             { header: 'Brand', accessorKey: 'brand_name' },
             { header: 'Quantity', accessorKey: 'quantity' },
