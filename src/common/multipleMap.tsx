ff513a4d3afe30e27fa9f6d789d551a4b4f2a666
Debashish <debashish@slikk.club>
2024-12-11 23:17:46 +0530

    11-12-2024

--- src/common/multipleMap.tsx
+++ src/common/multipleMap.tsx
@@ -1,14 +1,17 @@
-import React, { useMemo, useState } from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useMemo, useState, useCallback, useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-import { FaMapMarkerAlt } from 'react-icons/fa' // You can use any icon library you prefer
+import { FaMapMarkerAlt } from 'react-icons/fa'
+import axios from 'axios'
+import _ from 'lodash' // Lodash for debounce
 
 // Fix for default icon issues with leaflet
 import icon from 'leaflet/dist/images/marker-icon.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png'
 
-let DefaultIcon = L.icon({
+const DefaultIcon = L.icon({
     iconUrl: icon,
     shadowUrl: iconShadow,
 })
@@ -28,11 +31,17 @@ interface MultipleMapProps {
     amount: any[]
 }
 
-const CurrentLocationButton = ({ setCenter }: { setCenter: React.Dispatch<React.SetStateAction<[number, number]>> }) => {
+interface CenterProps {
+    setCenter?: any
+    currLat: any
+    currLong: any
+}
+
+const CurrentLocationButton = ({ setCenter, currLat, currLong }: CenterProps) => {
     const map = useMap()
 
     const handleClick = () => {
-        map.setView([12.9014, 77.65122], 13) // Adjust the zoom level as needed
+        map.setView([currLat, currLong], 13)
     }
 
     return (
@@ -57,12 +66,67 @@ const CurrentLocationButton = ({ setCenter }: { setCenter: React.Dispatch<React.
 }
 
 const MultipleMap: React.FC<MultipleMapProps> = ({ latitudes, longitudes, amount }) => {
-    const currLat = 12.9014
-    const currLong = 77.65122
+    const [currLat, setCurrLat] = useState(12.9014)
+    const [currLong, setCurrLong] = useState(77.65122)
     const R = 6371
+    const [location, setLocation] = useState('')
+    const [suggestions, setSuggestions] = useState<any>([])
+    const [distanceBelowTen, setDistanceBelowTen] = useState<any[]>([])
+    const [distanceBelowTentoFifteen, setDistanceBelowTentoFifteen] = useState<any[]>([])
+    const [distanceBelowFifteenToThirty, setDistanceBelowFifteenToThirty] = useState<any[]>([])
+    const [distanceAboveThirty, setDistanceAboveThirty] = useState<any[]>([])
+
+    const handleSearchLocation = useCallback(
+        _.debounce(async (query: string) => {
+            if (!query) {
+                setSuggestions([])
+                return
+            }
+
+            try {
+                const response = await axios.get(`https://api.olamaps.io/places/v1/autocomplete`, {
+                    params: {
+                        input: query,
+                        language: 'English',
+                        api_key: 'mEHMu4ifYRDhxV4YArcu3AxxDF1AvjV9Hds9vANS',
+                    },
+                })
+
+                const data = response.data?.predictions || []
+                const names = data.map((result: any) => ({
+                    name: result.description,
+                    lat: result.geometry.location.lat,
+                    lng: result.geometry.location.lng,
+                }))
+
+                setSuggestions(names)
+            } catch (error) {
+                console.error('Error:', error)
+            }
+        }, 300),
+        [],
+    )
+
+    const handleChangeLocation = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+        setLocation(value)
+        handleSearchLocation(value)
+    }
+
+    const handleSelectSuggestion = (suggestion: any) => {
+        setCurrLat(suggestion.lat)
+        setCurrLong(suggestion.lng)
+        setSuggestions([])
+        setLocation(suggestion.name)
+    }
 
     const markers = useMemo(() => {
-        return latitudes.map((lat, index) => {
+        const belowTen: any[] = []
+        const tenToFifteen: any[] = []
+        const fifteenToThirty: any[] = []
+        const aboveThirty: any[] = []
+
+        const result = latitudes.map((lat, index) => {
             const lon = longitudes[index]
             const dLat = (lat - currLat) * (Math.PI / 180)
             const dLon = (lon - currLong) * (Math.PI / 180)
@@ -73,35 +137,91 @@ const MultipleMap: React.FC<MultipleMapProps> = ({ latitudes, longitudes, amount
             const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(rLat1) * Math.cos(rLat2)
             const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
 
-            const distance = (R * c).toFixed(2)
+            const distance = parseFloat((R * c).toFixed(2)) // Distance in km
+
+            if (distance <= 10) {
+                belowTen.push({ lat, lon, amount: amount[index], distance })
+            } else if (distance > 10 && distance <= 15) {
+                tenToFifteen.push({ lat, lon, amount: amount[index], distance })
+            } else if (distance > 15 && distance <= 30) {
+                fifteenToThirty.push({ lat, lon, amount: amount[index], distance })
+            } else {
+                aboveThirty.push({ lat, lon, amount: amount[index], distance })
+            }
+
             return { lat, lon, amount: amount[index], distance }
         })
-    }, [latitudes, longitudes, amount])
+
+        setDistanceBelowTen(belowTen)
+        setDistanceBelowTentoFifteen(tenToFifteen)
+        setDistanceBelowFifteenToThirty(fifteenToThirty)
+        setDistanceAboveThirty(aboveThirty)
+
+        return result
+    }, [latitudes, longitudes, amount, currLat, currLong, R])
+
+    const Belowdatas = [
+        { name: 'Below 10 km', value: distanceBelowTen },
+        { name: 'Between 10-15 km', value: distanceBelowTentoFifteen },
+        { name: 'Between 15-30 km', value: distanceBelowFifteenToThirty },
+        { name: 'Above 30 km', value: distanceAboveThirty },
+    ]
 
     return (
-        <MapContainer center={[currLat, currLong]} zoom={13} style={{ height: '100vh', width: '100%' }}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {markers.map((marker, index) => (
-                <Marker key={index} position={[marker.lat, marker.lon]}>
-                    <Popup>
-                        <div>
-                            <p>Amount: Rs.{marker.amount}</p>
-                            <p>Distance: {marker.distance} km</p>
+        <div className="flex flex-col gap-4">
+            <div>
+                <input className="rounded-xl" placeholder="Enter location" value={location} onChange={handleChangeLocation} />
+                {suggestions.length > 0 && (
+                    <ul className="mt-2 border rounded-xl bg-white shadow-md">
+                        {suggestions.map((suggestion: any, index: any) => (
+                            <li
+                                key={index}
+                                className="p-2 cursor-pointer hover:bg-gray-200"
+                                onClick={() => handleSelectSuggestion(suggestion)}
+                            >
+                                {suggestion.name}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+            <div className="flex flex-col gap-10 xl:flex-row">
+                <MapContainer center={[currLat, currLong]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    {markers.map((marker, index) => (
+                        <Marker key={index} position={[marker.lat, marker.lon]}>
+                            <Popup>
+                                <div>
+                                    <p>Amount: Rs.{marker.amount}</p>
+                                    <p>Distance: {marker.distance} km</p>
+                                </div>
+                            </Popup>
+                        </Marker>
+                    ))}
+
+                    <Marker position={[currLat, currLong]} icon={officeIcon}>
+                        <Popup>
+                            <div>
+                                <p>SlikkSync Technologies</p>
+                            </div>
+                        </Popup>
+                    </Marker>
+
+                    <CurrentLocationButton setCenter={() => {}} currLat={currLat} currLong={currLong} />
+                </MapContainer>
+                <div className="space-y-2  xl:w-[250px]">
+                    {Belowdatas.map((item, key) => (
+                        <div
+                            key={key}
+                            className="flex justify-between  items-center bg-white px-4 py-2 rounded-md border border-gray-200 shadow-sm"
+                        >
+                            <span className="font-medium text-gray-700">{item?.name}:</span>
+                            <span className="text-sm text-blue-700">{item?.value?.length}</span>
                         </div>
-                    </Popup>
-                </Marker>
-            ))}
-
-            <Marker position={[currLat, currLong]} icon={officeIcon}>
-                <Popup>
-                    <div>
-                        <p>SlikkSync Technologies</p>
-                    </div>
-                </Popup>
-            </Marker>
-
-            <CurrentLocationButton setCenter={() => {}} />
-        </MapContainer>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
