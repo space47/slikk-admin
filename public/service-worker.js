ac3461bca9fe45ecd5b668613a54444f11edc68a
Debashish <debashish@slikk.club>
2024-12-16 18:37:52 +0530

    16-12-2024

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,29 @@
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches.open('my-cache').then((cache) => {
-//             return cache.addAll(['/', 'index.html', '/manifest.json', 'icon.png']);
-//         }),
-//     );
-// });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll(['/', 'index.html', '/manifest.json', 'icon.png']);
+        }),
+    );
+});
 
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches.match(event.request).then((response) => {
-//             return response || fetch(event.request);
-//         }),
-//     );
-// });
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        }),
+    );
+});
 
-// self.addEventListener('activate', (event) => {
-//     event.waitUntil(
-//         caches.keys().then((cacheNames) => {
-//             return Promise.all(
-//                 cacheNames.map((cacheName) => {
-//                     if (cacheName !== 'my-cache') {
-//                         return caches.delete(cacheName);
-//                     }
-//                 }),
-//             );
-//         }),
-//     );
-// });
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== 'my-cache') {
+                        return caches.delete(cacheName);
+                    }
+                }),
+            );
+        }),
+    );
+});
