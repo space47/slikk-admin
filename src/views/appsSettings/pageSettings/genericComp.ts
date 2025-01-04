dd52919a98b2193fe43d13469eb44aba5c14f1a6
Debashish <debashish@slikk.club>
2025-01-04 13:34:32 +0530

    04-1-2025

--- src/views/appsSettings/pageSettings/genericComp.ts
+++ src/views/appsSettings/pageSettings/genericComp.ts
@@ -639,6 +639,22 @@ export const MobileAndDesktopPositions = [
         label: 'Bottom',
         value: 'bottom',
     },
+    {
+        label: 'Top-Left',
+        value: 'top-left',
+    },
+    {
+        label: 'Top-Right',
+        value: 'top-right',
+    },
+    {
+        label: 'Bottom-Left',
+        value: 'bottom-left',
+    },
+    {
+        label: 'Bottom-Right',
+        value: 'bottom-right',
+    },
 ]
 
 export const NameFieldArray = [
