fc14ba3e940a82ca277e3982759520eb604674b9
Debashish <debashish@slikk.club>
2025-01-11 14:20:46 +0530

    10-01-2025

--- src/views/sales/OrderDetails/components/Activity.tsx
+++ src/views/sales/OrderDetails/components/Activity.tsx
@@ -265,6 +265,9 @@ const Activity = ({ data = [], status, product = [], payment, invoice_id, logist
             const modalContent = mainData?.delivery_type === 'STANDARD' ? 'Mark as Shipped' : 'Out for Delivery'
             return { buttonText, modalContent }
         }
+        if (status === 'SHIPPED') {
+            return { buttonText: 'MARK AS DELIVERED' }
+        }
 
         switch (lastLogStatus) {
             case 'DELIVERY_CREATED': {
