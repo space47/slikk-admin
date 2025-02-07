9170e2cf22f2f48cbd0cae34658ac9480cd952e8
Debashish <debashish@slikk.club>
2025-02-07 17:34:27 +0530

    7-02-2025

--- src/views/slikkLogistics/riderDetails/RiderDetails.tsx
+++ src/views/slikkLogistics/riderDetails/RiderDetails.tsx
@@ -10,17 +10,21 @@ import { companyStore } from '@/store/types/companyStore.types'
 import { fetchCompanyStore } from '@/store/slices/companyStoreSlice/companyStore.slice'
 import { Select } from '@/components/ui'
 import { FaRegCircleDot } from 'react-icons/fa6'
+import UltimateDatePicker from '@/common/UltimateDateFilter'
+import { MdLocationOff, MdLocationOn, MdOutlineLocationOn } from 'react-icons/md'
 
 const RiderDetails = () => {
     const dispatch = useAppDispatch()
     const [riderDetails, setRiderDetails] = useState<RiderData[]>([])
+    const [from, setFrom] = useState(moment().format('YYYY-MM-DD'))
+    const [to, setTo] = useState(moment().format('YYYY-MM-DD'))
     const [showRiderDetailModal, setShowRiderDetailModal] = useState(false)
     const [mobileForParticularRider, setMobileForParticularRider] = useState<string>()
     const [currentStoreLocation, setCurrentStoreLocation] = useState<Record<string, number | undefined>>({
         lat: 12.9001879,
         long: 77.6510959,
     })
-
+    const [showRideeMap, setShowRiderMap] = useState<boolean>(false)
     const { storeResults } = useAppSelector<companyStore>((state) => state.companyStore)
 
     useEffect(() => {
@@ -113,33 +117,58 @@ const RiderDetails = () => {
         setShowRiderDetailModal(true)
         setMobileForParticularRider(mobile)
     }
+    const handleDateChange = (dates: [Date | null, Date | null] | null) => {
+        if (dates && dates[0]) {
+            setFrom(moment(dates[0]).format('YYYY-MM-DD'))
+            setTo(dates[1] ? moment(dates[1]).format('YYYY-MM-DD') : moment().format('YYYY-MM-DD'))
+        }
+    }
 
     return (
         <div>
             <div className="flex flex-col gap-10">
-                <div className="flex flex-col gap-3">
-                    <div className="text-xl font-bold">Select Warehouse:</div>
-                    <Select
-                        isClearable
-                        placeholder="select slikk store"
-                        options={formattedData}
-                        getOptionLabel={(option) => option.label}
-                        getOptionValue={(option) => option.value}
-                        className="w-1/2"
-                        onChange={(newVal) => {
-                            console.log('value of the data', newVal)
-                            setCurrentStoreLocation({
-                                lat: newVal?.value?.lat,
-                                long: newVal?.value?.long,
-                            })
-                        }}
-                    />
+                <div className="flex justify-between items-center">
+                    <div className="flex flex-col gap-3">
+                        <div className="text-xl font-bold">Select Warehouse:</div>
+                        <Select
+                            isClearable
+                            placeholder="select slikk store"
+                            options={formattedData}
+                            getOptionLabel={(option) => option.label}
+                            getOptionValue={(option) => option.value}
+                            className="w-full"
+                            onChange={(newVal) => {
+                                console.log('value of the data', newVal)
+                                setCurrentStoreLocation({
+                                    lat: newVal?.value?.lat,
+                                    long: newVal?.value?.long,
+                                })
+                            }}
+                        />
+                    </div>
+
+                    <div className="flex gap-2 items-center">
+                        <div>
+                            <UltimateDatePicker from={from} setFrom={setFrom} to={to} setTo={setTo} handleDateChange={handleDateChange} />
+                        </div>
+                        <div onClick={() => setShowRiderMap((prev) => !prev)} className="items-center xl:mt-8">
+                            <button>
+                                {showRideeMap ? (
+                                    <MdLocationOff className="text-4xl text-red-700 " />
+                                ) : (
+                                    <MdLocationOn className="text-4xl text-green-600 " />
+                                )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="xl:w-[90%]  items-center">
-                    <div className="text-xl font-bold">Full Rider Location</div>
-                    <RiderFullMap riderDetails={riderDetails} currentStore={currentStoreLocation} />
-                </div>
+                {showRideeMap && (
+                    <div className="xl:w-[90%]  items-center">
+                        <div className="text-xl font-bold">Full Rider Location</div>
+                        <RiderFullMap riderDetails={riderDetails} currentStore={currentStoreLocation} />
+                    </div>
+                )}
 
                 <div className="flex flex-col gap-3">
                     <div className="text-xl font-bold">Rider Tables:</div>
