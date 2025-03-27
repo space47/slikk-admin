235762eee9a66da443c0a693268b1d52870a029e
Debashish <debashish@slikk.club>
2025-03-27 21:29:27 +0530

    Fixed config

--- src/views/sales/OrderDetails/OrderDetails.tsx
+++ src/views/sales/OrderDetails/OrderDetails.tsx
@@ -252,6 +252,16 @@ const OrderDetails = () => {
                                         </div>
                                     </div>
                                 )}
+                                {data?.reference_return && (
+                                    <div>
+                                        <a
+                                            href={`/app/returnOrders/${data?.reference_return}`}
+                                            className="text-blue-600 hover:underline hover:text-blue-800 transition duration-200"
+                                        >
+                                            <span className="text-gray-700">Return Order:</span> {data?.reference_return}
+                                        </a>
+                                    </div>
+                                )}
                                 {data?.exchange_order_id?.length > 0 && (
                                     <div className="flex flex-col xl:flex-row gap-2 items-center">
                                         <span className="text-gray-700">Exchange Orders:</span>
