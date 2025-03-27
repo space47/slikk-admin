b1302ddef3d583c1b91a495e18c0d5e2c5c46dc4
Debashish <debashish@slikk.club>
2025-03-27 23:47:25 +0530

    Fixed config

--- src/views/inventory-management/inward/inward.tsx
+++ src/views/inventory-management/inward/inward.tsx
@@ -26,7 +26,7 @@ const PaginationTable = () => {
         try {
             let filter = ''
             if (globalFilter) {
-                filter = `&mobile=${globalFilter}`
+                filter = `&brand=${globalFilter}`
             }
             const response = await axiosInstance.get(
                 `goods/received/${selectedCompany.id}?p=${page}&page_size=${pageSize}${filter}`, // &company_id
