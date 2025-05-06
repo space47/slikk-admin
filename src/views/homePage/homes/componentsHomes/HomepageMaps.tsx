9775596d71cec26515a32b53503cc479982a111b
Debashish <debashish@slikk.club>
2025-05-06 13:22:53 +0530

    Separated Map Component

---
+++ src/views/homePage/homes/componentsHomes/HomepageMaps.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import MultipleMap from '@/common/multipleMap'
+import axioisInstance from '@/utils/intercepter/globalInterceptorSetup'
+import UserMap from '@/views/analytics/userAnalytics/UserMap'
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+
+interface props {
+    from: string
+    to: string
+    activeTab: string
+    setAccessDenied: (value: boolean) => void
+}
+
+const HomepageMaps = ({ from, to, activeTab, setAccessDenied }: props) => {
+    const [orders, setOrders] = useState<any[]>([])
+    const To_Date = moment(to).add(1, 'days').format('YYYY-MM-DD')
+
+    useEffect(() => {
+        const fetchOrderForLocation = async () => {
+            try {
+                const response = await axioisInstance.get(`/merchant/orders?location_data=true&from=${from}&to=${To_Date}`)
+                const ordersData = response.data?.data
+                setOrders(ordersData)
+            } catch (error: any) {
+                if (error.response && error.response.status === 403) {
+                    setAccessDenied(true)
+                }
+                console.log(error)
+            }
+        }
+        fetchOrderForLocation()
+    }, [from, to])
+    return (
+        <div>
+            {activeTab === 'orders' && (
+                <div className="bg-white p-6 rounded-xl shadow-md">
+                    <MultipleMap
+                        latitudes={orders.map((item) => item.latitude || [])}
+                        longitudes={orders.map((item) => item.longitude || [])}
+                        amount={orders.map((item) => item.amount || [])}
+                        currentStatus={orders.map((item) => item.status || [])}
+                        currentInvoice={orders.map((item) => item.invoice_id || [])}
+                    />
+                </div>
+            )}
+            {activeTab === 'users' && (
+                <div className="bg-white p-6 rounded-xl shadow-md mt-10">
+                    <UserMap from={from} to={To_Date} />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default HomepageMaps
