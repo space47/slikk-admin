7796ad3a0803a620abb24ce8ab6745127316d4d1
Debashish <debashish@slikk.club>
2025-06-21 16:40:22 +0530

    Added Product count in sub category

--- src/views/category-management/subCategory/subCategoryUtils/useGetSubCategory.tsx
+++ src/views/category-management/subCategory/subCategoryUtils/useGetSubCategory.tsx
@@ -1,5 +1,6 @@
 import { useAppSelector } from '@/store'
 import { DIVISION_STATE } from '@/store/types/division.types'
+import { SUBCATEGORY_STATE } from '@/store/types/subcategory.types'
 
 interface props {
     selectedDivision: string
@@ -9,15 +10,19 @@ interface props {
 export const useGetSubCategory = ({ selectedCategory, selectedDivision }: props) => {
     const divisions = useAppSelector<DIVISION_STATE>((state) => state.division)
     const DivisionArray = divisions?.divisions?.map((item) => item?.name)
+    const subCategoryArray = useAppSelector<SUBCATEGORY_STATE>((state) => state.subCategory)
 
     let subCategories = []
 
     const division = divisions?.divisions?.find((d) => d?.name === selectedDivision)
     const category = division?.categories?.find((c) => c?.name === selectedCategory)
+    const subCategoryProductCount = subCategoryArray?.subcategories?.find((c) => c.category_name === selectedCategory)?.product_count
+
     if (selectedDivision !== 'Select Division' && selectedCategory !== 'Select Category') {
         subCategories =
             category?.sub_categories?.map((subCat) => ({
                 ...subCat,
+                product_count: subCategoryProductCount,
                 division_name: selectedDivision,
                 category_name: selectedCategory,
             })) || []
@@ -26,6 +31,7 @@ export const useGetSubCategory = ({ selectedCategory, selectedDivision }: props)
             division?.categories?.flatMap((category) =>
                 (category?.sub_categories || []).map((subCat) => ({
                     ...subCat,
+                    product_count: subCategoryProductCount,
                     division_name: selectedDivision,
                     category_name: category.name,
                 })),
@@ -35,21 +41,13 @@ export const useGetSubCategory = ({ selectedCategory, selectedDivision }: props)
             divisions?.divisions?.flatMap((division) =>
                 (category?.sub_categories || []).map((subCat) => ({
                     ...subCat,
+                    product_count: subCategoryProductCount,
                     division_name: division.name,
                     category_name: selectedCategory,
                 })),
             ) || []
     } else {
-        subCategories =
-            divisions?.divisions?.flatMap((division) =>
-                division?.categories?.flatMap((category) =>
-                    (category?.sub_categories || []).map((subCat) => ({
-                        ...subCat,
-                        division_name: division.name,
-                        category_name: category.name,
-                    })),
-                ),
-            ) || []
+        subCategories = subCategoryArray?.subcategories || []
     }
 
     return { subCategories, DivisionArray }
