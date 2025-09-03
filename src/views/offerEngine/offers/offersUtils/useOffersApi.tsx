4fa23533948064107dcec0bf743af9b519b3c4ad
Debasishe <debashish@slikk.club>
2025-09-03 08:31:10 -0700

    offers

--- src/views/offerEngine/offers/offersUtils/useOffersApi.tsx
+++ src/views/offerEngine/offers/offersUtils/useOffersApi.tsx
@@ -1,11 +1,11 @@
 import { useAppDispatch, useAppSelector } from '@/store'
 import { offersService } from '@/store/services/offersService'
-import { OffersInitialStateType, setOffersData, setPage, setPageSize } from '@/store/slices/offerSlice/offerSlice'
+import { OffersInitialStateType, setOffersData, setPage, setPageSize, setCount } from '@/store/slices/offerSlice/offerSlice'
 import { useEffect } from 'react'
 
 export const useOffersApi = () => {
     const dispatch = useAppDispatch()
-    const { offers, page, pageSize } = useAppSelector<OffersInitialStateType>((state) => state.offers)
+    const { offers, page, pageSize, count } = useAppSelector<OffersInitialStateType>((state) => state.offers)
     const { data, isSuccess, isError, error } = offersService.useOffersQuery(
         {
             page: page,
@@ -18,9 +18,10 @@ export const useOffersApi = () => {
 
     useEffect(() => {
         if (isSuccess && data) {
-            dispatch(setOffersData(data.body?.data || []))
+            dispatch(setOffersData(data.body?.data?.offers || []))
+            dispatch(setCount(data.body?.data?.total || 0))
         }
     }, [isSuccess, data])
 
-    return { offers, page, pageSize, setPage, setPageSize, isError, error }
+    return { offers, page, pageSize, setPage, setPageSize, isError, error, count }
 }
