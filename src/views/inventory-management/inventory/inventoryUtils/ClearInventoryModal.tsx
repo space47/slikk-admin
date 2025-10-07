f6c1448371a599c07a49f5c15629f6808b60d1c4
Debasishe <debashish@slikk.club>
2025-10-07 22:18:23 +0530

    clear inventory

---
+++ src/views/inventory-management/inventory/inventoryUtils/ClearInventoryModal.tsx
@@ -0,0 +1,139 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Dialog, FormContainer, FormItem, Select } from '@/components/ui'
+import FormButton from '@/components/ui/Button/FormButton'
+import { useAppDispatch, useAppSelector } from '@/store'
+import { getAllBrandsAPI } from '@/store/action/brand.action'
+import { BRAND_STATE } from '@/store/types/brand.types'
+import { CATEGORY_STATE } from '@/store/types/category.types'
+import { SINGLE_COMPANY_DATA } from '@/store/types/company.types'
+import { SUBCATEGORY_STATE } from '@/store/types/subcategory.types'
+import { filterEmptyValues } from '@/utils/apiBodyUtility'
+import axioisInstance from '@/utils/intercepter/globalInterceptorSetup'
+import { errorMessage, successMessage } from '@/utils/responseMessages'
+import { notification } from 'antd'
+import { AxiosError } from 'axios'
+import { Field, Form, Formik } from 'formik'
+import React, { useEffect, useState } from 'react'
+
+interface props {
+    isOpen: boolean
+    setIsOpen: (x: boolean) => void
+    storeId: number
+}
+
+const ClearInventoryModal = ({ isOpen, setIsOpen, storeId }: props) => {
+    const companyList = useAppSelector<SINGLE_COMPANY_DATA[]>((state) => state.company.company)
+    const category = useAppSelector<CATEGORY_STATE>((state) => state.category)
+    const subCategory = useAppSelector<SUBCATEGORY_STATE>((state) => state.subCategory)
+    const brands = useAppSelector<BRAND_STATE>((state) => state.brands)
+    const [spinner, setSpinner] = useState(false)
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(getAllBrandsAPI())
+    }, [dispatch])
+
+    const handleSubmit = async (values: any) => {
+        if (!values?.companyList) {
+            notification.error({ message: 'Company is required' })
+            return
+        }
+        const body = {
+            store_id: storeId,
+            brand_id: values?.brand?.id || '',
+            company_id: values?.companyList?.map((item: any) => item?.id)?.join(','),
+            category: values?.category?.name || '',
+            subcategory: values?.sub_categories?.name || '',
+        }
+        const filteredBody = filterEmptyValues(body)
+        try {
+            const res = await axioisInstance.post(`/inventory-location/bulk/clear`, filteredBody)
+            successMessage(res)
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                errorMessage(error)
+            }
+        } finally {
+            setSpinner(false)
+            setIsOpen(false)
+        }
+    }
+
+    return (
+        <Dialog isOpen={isOpen} onClose={() => setIsOpen(false)} width={800}>
+            <h4 className="text-red-500 mb-4"> Clear Inventory From Location</h4>
+            <Formik enableReinitialize initialValues={{} as any} onSubmit={handleSubmit}>
+                {({ setFieldValue }) => (
+                    <Form>
+                        <FormContainer>
+                            <FormItem label="Company" asterisk>
+                                <Field name="companyList">
+                                    {() => {
+                                        return (
+                                            <Select
+                                                isMulti
+                                                className="w-full"
+                                                options={companyList}
+                                                getOptionLabel={(option) => option.name}
+                                                getOptionValue={(option) => option.id?.toString()}
+                                                onChange={(newVal) => {
+                                                    const selectedValues = newVal
+                                                    setFieldValue('companyList', selectedValues)
+                                                }}
+                                            />
+                                        )
+                                    }}
+                                </Field>
+                            </FormItem>
+
+                            <FormItem label="Brand">
+                                <div className="flex flex-col xl:flex-row items-start gap-2">
+                                    <Select
+                                        options={brands.brands}
+                                        getOptionLabel={(option) => option.name}
+                                        getOptionValue={(option) => option.id.toString()}
+                                        className="w-full"
+                                        onChange={(val) => {
+                                            setFieldValue('brand', val)
+                                        }}
+                                    />
+                                </div>
+                            </FormItem>
+                            <FormItem label="Category">
+                                <div className="flex flex-col xl:flex-row items-start gap-2">
+                                    <Select
+                                        options={category.categories}
+                                        getOptionLabel={(option) => option.name}
+                                        getOptionValue={(option) => option?.id?.toString()}
+                                        className="w-full"
+                                        onChange={(val) => {
+                                            setFieldValue('category', val)
+                                        }}
+                                    />
+                                </div>
+                            </FormItem>
+                            <FormItem label="Sub Category">
+                                <div className="flex flex-col xl:flex-row items-start gap-2">
+                                    <Select
+                                        options={subCategory.subcategories}
+                                        getOptionLabel={(option) => option.name}
+                                        getOptionValue={(option) => option?.id?.toString()}
+                                        className="w-full"
+                                        onChange={(val) => {
+                                            setFieldValue('sub_categories', val)
+                                        }}
+                                    />
+                                </div>
+                            </FormItem>
+                        </FormContainer>
+
+                        <FormButton isSpinning={spinner} value="Apply" />
+                    </Form>
+                )}
+            </Formik>
+        </Dialog>
+    )
+}
+
+export default ClearInventoryModal
