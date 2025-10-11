ce8e255ff86141a70286d093864f36faf799035d
Debasishe <debashish@slikk.club>
2025-10-11 22:04:20 +0530

    rider fix too

--- src/views/slikkLogistics/riderDetails/AddRiders/AddRider.tsx
+++ src/views/slikkLogistics/riderDetails/AddRiders/AddRider.tsx
@@ -66,6 +66,9 @@ const AddRider = () => {
                 is_active: riderProfile[0]?.is_active || false,
                 lat: riderProfile[0]?.service_latitude,
                 long: riderProfile[0]?.service_longitude,
+                agency: riderProfile[0]?.agency,
+                rider_delivery_type: riderProfile[0]?.rider_delivery_type,
+                // store: riderProfile[0]?.store?.map((item: any) => item?.id),
             }
         }
         return {
