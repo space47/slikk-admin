ba7cc0bb075ea4471eabc26ef35c306606ff684d
Debasishe <debashish@slikk.club>
2025-10-13 19:35:21 +0530

    sub page extra fields

---
+++ src/views/appsSettings/pageSubPage/pageSubPageUtils/subPageCommon.ts
@@ -0,0 +1,32 @@
+export const Extra_Fields_Subpage = [
+    {
+        name: 'extra_attributes.icon_color',
+        label: 'Icon Color',
+        type: 'text',
+    },
+    {
+        name: 'extra_attributes.icon_color_web',
+        label: 'Icon Color (Web)',
+        type: 'text',
+    },
+    {
+        name: 'extra_attributes.address_header_color_web',
+        label: 'Address Header Color (Web)',
+        type: 'text',
+    },
+    {
+        name: 'extra_attributes.address_header_color',
+        label: 'Address Header Color',
+        type: 'text',
+    },
+    {
+        name: 'extra_attributes.address_description_color',
+        label: 'Address Description Color',
+        type: 'text',
+    },
+    {
+        name: 'extra_attributes.address_description_color_web',
+        label: 'Address Description Color (Web)',
+        type: 'text',
+    },
+]
