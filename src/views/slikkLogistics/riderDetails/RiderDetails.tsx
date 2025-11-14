52c8d8fbd3232767ec2f75fdcc5a8326bbb6f391
Debasishe <debashish@slikk.club>
2025-11-14 17:29:29 +0530

    rider service and order changes

--- src/views/slikkLogistics/riderDetails/RiderDetails.tsx
+++ src/views/slikkLogistics/riderDetails/RiderDetails.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useMemo, useState, useCallback } from 'react'
 import moment from 'moment'
@@ -62,6 +63,7 @@ const RiderDetails = () => {
     const [riderDownload, riderDownloadResponse] = ridersService.useLazyRiderDetailsDownloadQuery()
     const { debounceFilter } = useDebounceInput({ globalFilter: globalFilter as string, delay: DEBOUNCE_DELAY })
     const [deleteRider, deleteResponse] = ridersService.useRiderDeleteMutation()
+    const [performanceDownload, performanceResponse] = ridersService.useLazyRiderPerformanceDownloadQuery()
     const {
         data: riders,
         isSuccess,
@@ -129,6 +131,20 @@ const RiderDetails = () => {
             notification.error({ message: 'Failed to download rider data' })
         }
     }, [riderDownloadResponse.isError, riderDownloadResponse?.isSuccess])
+
+    useEffect(() => {
+        if (performanceResponse.isSuccess) {
+            notification.success({
+                message: performanceResponse.data?.message || 'File is sent to your registered mail',
+            })
+        }
+
+        if (performanceResponse.isError) {
+            notification.error({
+                message: 'Failed to download rider data',
+            })
+        }
+    }, [performanceResponse.isSuccess, performanceResponse.isError])
 
     const handleActiveCareer = useCallback((id: number, e: any, checked: boolean, mobile: string, name: string) => {
         setMobileForParticularRider(mobile)
@@ -205,6 +221,14 @@ const RiderDetails = () => {
 
     const handleDeleteRider = () => {
         deleteRider({ mobile: currentRow?.profile?.mobile as number })
+    }
+
+    const handlePerformance = () => {
+        performanceDownload({
+            from,
+            to: moment(to).add(1, 'days').format('YYYY-MM-DD'),
+            _forceReset: Date.now(),
+        } as any)
     }
 
     const columns = RiderColumns({
@@ -334,13 +358,16 @@ const RiderDetails = () => {
                         )}
                         <div className="xl:mt-8" onClick={handleCopyLink}>
                             <a
-                                className="p-2 rounded-xl bg-gradient-to-r bg-blue-700/80 text-white no-underline flex gap-2 font-bold backdrop-blur-sm"
+                                className="px-4 py-2 text-white rounded-lg border border-gray-600 bg-gray-500 text-gray-800 no-underline flex gap-2 items-center hover:bg-gray-400 transition"
                                 href="https://slikk-dev-assets-public.s3.ap-south-1.amazonaws.com/builds/Rider+App/rider-app-new.apk"
                             >
                                 App Link
                             </a>
                         </div>
 
+                        <Button variant="twoTone" color="gray" icon={<FaDownload />} className="xl:mt-8" onClick={handlePerformance}>
+                            Rider Performance Report
+                        </Button>
                         <UltimateDatePicker
                             dispatch={dispatch}
                             from={from}
