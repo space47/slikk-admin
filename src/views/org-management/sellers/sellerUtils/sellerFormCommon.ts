97d68f36ce7bca45aa64135aaf42727cf5868e95
Debasishe <debashish@slikk.club>
2025-11-15 21:25:04 +0530

    seller fields

--- src/views/org-management/sellers/sellerUtils/sellerFormCommon.ts
+++ src/views/org-management/sellers/sellerUtils/sellerFormCommon.ts
@@ -1,10 +1,10 @@
 export const BasicSellerInformation = [
     { label: 'Vendor Business Name', name: 'registered_name', type: 'text', isRequired: true, visible: true },
     { label: 'Company Name', name: 'name', type: 'text', isRequired: true, visible: true },
-    { label: 'Contact Person Name', name: 'head_name', type: 'text', isRequired: false, visible: true },
+    { label: 'Contact Person Name', name: 'head_name', type: 'text', isRequired: true, visible: true },
     { label: 'Vendor Code', name: 'vendor_code', type: 'text', isRequired: true, visible: true },
     { label: 'Email Address', name: 'head_email', type: 'text', isRequired: false, visible: true },
-    { label: 'Phone Number', name: 'head_number', type: 'number', isRequired: false, visible: true },
+    { label: 'Phone Number', name: 'head_number', type: 'number', isRequired: true, visible: true },
 ]
 export const BusinessDetails = [
     { label: 'PAN Number', name: 'pan_number', type: 'text', isRequired: true, visible: true },
