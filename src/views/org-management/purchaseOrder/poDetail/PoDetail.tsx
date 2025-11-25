c59f473ec08812fb15d383be6c78723c40ef2b31
Debasishe <debashish@slikk.club>
2025-11-25 16:11:31 +0530

    added po details

--- src/views/org-management/purchaseOrder/poDetail/PoDetail.tsx
+++ src/views/org-management/purchaseOrder/poDetail/PoDetail.tsx
@@ -1,7 +1,107 @@
-import React from 'react'
+import EasyTable from '@/common/EasyTable'
+import { Button, Card, Spinner } from '@/components/ui'
+import { getApiErrorMessage } from '@/constants/generateErrorMessage'
+import { purchaseOrderService } from '@/store/services/purchaseOrderService'
+import { PurchaseOrderItem, PurchaseOrderTable } from '@/store/types/po.types'
+import { notification } from 'antd'
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useOrderItemColumns } from '../poUtils/useOrderItemColumns'
+import PageCommon from '@/common/PageCommon'
+import LoadingSpinner from '@/common/LoadingSpinner'
+import { usePoDetailUi } from './usePoDetailUi'
 
 const PoDetail = () => {
-    return <div>PoDetail</div>
+    const { purchase_id } = useParams()
+    const [page, setPage] = useState(1)
+    const [pageSize, setPageSize] = useState(10)
+    const [count, setCount] = useState(0)
+    const [purchaseDetail, setPurchaseDetail] = useState<PurchaseOrderTable>()
+    const [lineItems, setLineItems] = useState<PurchaseOrderItem[]>([])
+    const { data, isSuccess, isError, isLoading, error } = purchaseOrderService.usePurchaseSingleOrdersListQuery({
+        order_id: purchase_id,
+    })
+    const WaitStatus = purchaseDetail?.status?.toLowerCase() === 'created' || purchaseDetail?.status?.toLowerCase() === 'waiting_approval'
+    const {
+        data: itemsData,
+        isSuccess: itemSuccess,
+        isError: itemIsError,
+        error: itemError,
+        isFetching: itemsFetching,
+        isLoading: itemsLoading,
+    } = purchaseOrderService.useOrderItemsQuery({ purchase_order_id: purchase_id as string, page, pageSize }, { skip: !purchase_id })
+
+    useEffect(() => {
+        if (isSuccess) setPurchaseDetail(data?.data)
+        if (isError) {
+            const message = getApiErrorMessage(error)
+            notification.error({ message })
+        }
+    }, [isSuccess, isError, error, data?.data])
+
+    useEffect(() => {
+        if (itemSuccess) {
+            setLineItems(itemsData?.data?.results)
+            setCount(itemsData?.data?.count)
+        }
+        if (itemIsError) {
+            const message = getApiErrorMessage(itemError) || 'Failed to load'
+            notification.error({ message })
+        }
+    }, [itemSuccess, itemIsError])
+
+    const { ActivityBar, ButtonUI, OrderInformation, VendorInformation } = usePoDetailUi({
+        purchaseDetail: purchaseDetail as PurchaseOrderTable,
+    })
+
+    const columns = useOrderItemColumns({})
+
+    if (isLoading) {
+        return <LoadingSpinner />
+    }
+
+    return (
+        <div className="mt-5 flex flex-col gap-5">
+            <div className="flex flex-col gap-4 xl:flex-row xl:justify-between">
+                <div className="flex flex-col gap-3">
+                    <div className="flex gap-2 items-center">
+                        <h5>PO-{purchaseDetail?.id}</h5>
+                        <span className="bg-gray-100 p-2 rounded-xl">{purchaseDetail?.status}</span>
+                    </div>
+                    <p>Created on {moment(purchaseDetail?.created_at).format('YYYY-MM-DD HH:mm:ss a')}</p>
+                </div>
+                {WaitStatus && <div>{ButtonUI()}</div>}
+                <div>
+                    <Button variant="twoTone" color="yellow">
+                        Export Pdf
+                    </Button>
+                </div>
+            </div>
+            <div>{ActivityBar()}</div>
+            <div className="flex xl:flex-row md:flex-row flex-col  gap-4 w-full items-stretch">
+                <div className="flex-1">
+                    <VendorInformation />
+                </div>
+                <div className="flex-1">
+                    <OrderInformation />
+                </div>
+            </div>
+            <Card className="p-2 shadow-xl">
+                <div className="mt-2 mb-5">
+                    <h4>Line Items</h4>
+                </div>
+                {itemsLoading ||
+                    (itemsFetching && (
+                        <div className="flex items-center justify-between">
+                            <Spinner size={30} />
+                        </div>
+                    ))}
+                <EasyTable overflow columns={columns} mainData={lineItems} page={page} pageSize={pageSize} />
+                <PageCommon page={page} pageSize={pageSize} setPage={setPage} setPageSize={setPageSize} totalData={count} />
+            </Card>
+        </div>
+    )
 }
 
 export default PoDetail
---
+++ src/views/org-management/purchaseOrder/poDetail/usePoDetailUi.tsx
@@ -0,0 +1,93 @@
+import { Button, Card } from '@/components/ui'
+import { PurchaseOrderTable } from '@/store/types/po.types'
+import React from 'react'
+import { FaCheck, FaRegCommentDots } from 'react-icons/fa'
+
+interface Props {
+    purchaseDetail: PurchaseOrderTable
+}
+
+export const usePoDetailUi = ({ purchaseDetail }: Props) => {
+    const ButtonUI = () => {
+        return (
+            <div className="flex xl:flex-row flex-col gap-3 items-center">
+                <Button variant="blue" size="sm" icon={<FaCheck />}>
+                    Approve and Send to Vendor
+                </Button>
+                <Button variant="blue" size="sm" icon={<FaRegCommentDots />}>
+                    Add Comments
+                </Button>
+            </div>
+        )
+    }
+
+    const ActivityBar = () => {
+        const ApproverDetail = [
+            { label: 'Created By', value: purchaseDetail?.poc_name },
+            { label: 'Approver Name', value: purchaseDetail?.approver_name },
+        ]
+        return (
+            <Card className="shadow-xl">
+                <div className="flex justify-between">
+                    <div>
+                        <h6>Status Timeline</h6>
+                    </div>
+                    <div className="flex flex-col gap-2 text-sm">
+                        {ApproverDetail?.map((item, key) => (
+                            <div key={key} className="flex justify-between items-center py-1 gap-2">
+                                <span className="text-blue-500 font-bold">{item?.label}:</span>
+                                <span className="text-gray-800 font-semibold">{item?.value || '-'}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </Card>
+        )
+    }
+
+    const VendorInformation = () => {
+        const VendorDataArray = [
+            { label: 'Vendor Name', value: purchaseDetail?.brand_name },
+            { label: 'Vendor GST', value: purchaseDetail?.vendor_gst_no },
+            { label: 'Vendor Address', value: purchaseDetail?.vendor_address },
+        ]
+
+        return (
+            <Card className="shadow-xl h-full">
+                <div>
+                    <h4>Vendor Information</h4>
+                    {VendorDataArray?.map((item, key) => (
+                        <div key={key} className="flex h-full justify-between items-center py-1 gap-2 mt-5">
+                            <span className="text-gray-500 font-bold">{item?.label}:</span>
+                            <span className="text-gray-800 font-semibold">{item?.value || '-'}</span>
+                        </div>
+                    ))}
+                </div>
+            </Card>
+        )
+    }
+    const OrderInformation = () => {
+        const OrderDataArray = [
+            { label: 'Order Billing Address', value: purchaseDetail?.order_billing_address },
+            { label: 'Order Shipping Address', value: purchaseDetail?.order_shipping_address },
+            { label: 'Total Items', value: purchaseDetail?.total_count },
+            { label: 'Total Amount', value: purchaseDetail?.total_amount },
+        ]
+
+        return (
+            <Card className="shadow-xl h-full">
+                <div>
+                    <h4>Order Summary</h4>
+                    {OrderDataArray?.map((item, key) => (
+                        <div key={key} className="flex h-full justify-between items-center py-1 gap-2 mt-5">
+                            <span className="text-gray-500 font-bold">{item?.label}:</span>
+                            <span className="text-gray-800 font-semibold">{item?.value || '-'}</span>
+                        </div>
+                    ))}
+                </div>
+            </Card>
+        )
+    }
+
+    return { ButtonUI, ActivityBar, VendorInformation, OrderInformation }
+}
