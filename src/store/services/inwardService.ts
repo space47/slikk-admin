657010d76a4fd551f6911dea264b199cd9e08fba
Debasishe <debashish@slikk.club>
2026-01-03 16:36:52 +0530

    added inward types and services

---
+++ src/store/services/inwardService.ts
@@ -0,0 +1,98 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import RtkQueryService from '@/services/RtkQueryService'
+import { GRNDetails, GrnItemsResponseType, GrnResponseType, InwardParamType } from '../types/inward.types'
+
+export const inwardService = RtkQueryService.injectEndpoints({
+    endpoints: (builder) => ({
+        inwardDataGet: builder.query<GrnResponseType, InwardParamType>({
+            query: (params) => {
+                const parameters: Record<string, string | string[] | number> = {}
+                if (params.document_number) parameters.document_number = params.document_number
+                if (params.company) parameters.company_code = params.company
+                if (params.page) parameters.p = params.page.toString()
+                if (params.pageSize) parameters.page_size = params.pageSize.toString()
+                if (params.store_id) parameters.store_id = params.store_id
+
+                return {
+                    url: `goods/received/${params.id}`,
+                    method: 'GET',
+                    params: parameters,
+                }
+            },
+        }),
+        inwardSingleDetails: builder.query<{ data: GRNDetails }, { id?: string; gdn_number?: string }>({
+            query: (params) => {
+                const parameters: Record<string, string | string[]> = {}
+                if (params.gdn_number) parameters.gdn_number = params.gdn_number.toString()
+                return {
+                    url: `goods/received`,
+                    method: 'GET',
+                    params: parameters,
+                }
+            },
+        }),
+        gdnItemsDetails: builder.query<GrnItemsResponseType, { grn_number?: string; page: number; pageSize: number; sku?: string }>({
+            query: (params) => {
+                const parameters: Record<string, string | string[] | number> = {}
+                if (params.grn_number) parameters.grn_number = params.grn_number
+                if (params.page) parameters.p = params.page
+                if (params.pageSize) parameters.page_size = params.pageSize
+                if (params.sku) parameters.sku = params.sku
+
+                return {
+                    url: `/goods/qualitycheck`,
+                    method: 'GET',
+                    params: parameters,
+                }
+            },
+        }),
+        // createShipment: builder.query<{ status: string; data: CreateShipmentDetails }, { id: string | number }>({
+        //     query: (params) => {
+        //         return {
+        //             url: `/goods/dispatch/shipment/create/${params?.id}`,
+        //             method: 'GET',
+        //         }
+        //     },
+        // }),
+        // regenerateGrn: builder.query<any, { id: string; document_number?: string }>({
+        //     query: (params) => {
+        //         return {
+        //             url: `/goods/dispatch/${params.id}/detail`,
+        //             method: 'GET',
+        //             params: {
+        //                 download: true,
+        //                 regenerate: true,
+        //                 document_number: params.document_number,
+        //             },
+        //         }
+        //     },
+        // }),
+        // syncGdn: builder.mutation<{ status: string }, Record<string, any>>({
+        //     query: (body) => {
+        //         return {
+        //             url: `/goods/dispatch-synctoinventory`,
+        //             method: 'POST',
+        //             body,
+        //         }
+        //     },
+        // }),
+        // updateGdn: builder.mutation<{ status: string; message: string }, UpdateGdnArgs>({
+        //     query: ({ id, data }) => {
+        //         return {
+        //             url: `/goods/dispatch/${id}`,
+        //             method: 'PATCH',
+        //             body: data,
+        //         }
+        //     },
+        // }),
+        // addNewGdn: builder.mutation<{ status: string; message: string }, Record<string, string | number>>({
+        //     query: (body) => {
+        //         return {
+        //             url: `/goods/dispatch/`,
+        //             method: 'POST',
+        //             body,
+        //         }
+        //     },
+        // }),
+    }),
+})
