440ec1bbf300701c8589a55866a284d1cd9ee848
Debasishe <debashish@slikk.club>
2026-01-13 13:53:33 +0530

    created a service for order details

--- src/store/services/newOrderaService.ts
+++ src/store/services/newOrderaService.ts
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import RtkQueryService from '@/services/RtkQueryService'
-import { NewOrderResponseType } from '../types/newOrderTypes'
+import { NewOrderResponseType, Order } from '../types/newOrderTypes'
 
 type QueryParams = Record<string, string | number | boolean | undefined | any>
 
 export const newOrderService = RtkQueryService.injectEndpoints({
     endpoints: (builder) => ({
         getNewOrders: builder.query<NewOrderResponseType, QueryParams>({
+            query: (params) => ({
+                url: '/merchant/orders',
+                method: 'GET',
+                params,
+            }),
+            keepUnusedDataFor: 0,
+        }),
+        getOrderDetails: builder.query<Order, QueryParams>({
             query: (params) => ({
                 url: '/merchant/orders',
                 method: 'GET',
