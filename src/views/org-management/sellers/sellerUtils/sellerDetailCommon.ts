5637a4a939c0993276d096e27bdec9a44ee82970
Debasishe <debashish@slikk.club>
2026-02-19 17:58:30 +0530

    added enums in seller details

--- src/views/org-management/sellers/sellerUtils/sellerDetailCommon.ts
+++ src/views/org-management/sellers/sellerUtils/sellerDetailCommon.ts
@@ -1,89 +1,95 @@
+import { SellerKeys } from '../sellerCommon'
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const SellerDetailCommon = ({ seller: sellerData }: any) => {
     console.log('seller data', sellerData)
     const BasicSellerInformationDetail = [
-        { label: 'Vendor Business Name', name: 'registered_name', value: sellerData?.registered_name, visible: true },
-        { label: 'Company Name', name: 'name', value: sellerData?.name, visible: true },
-        { label: 'Contact Person Name', name: 'head_name', value: sellerData?.head_name, visible: true },
-        { label: 'Vendor Code', name: 'code', value: sellerData?.code, visible: true },
-        { label: 'Email Address', name: 'head_email', value: sellerData?.head_email, visible: true },
-        { label: 'Phone Number', name: 'head_contact', value: sellerData?.head_contact, visible: true },
-        { label: 'Fashion Style', name: 'segment', value: sellerData?.segment, visible: true },
+        { label: 'Vendor Business Name', name: SellerKeys.REGISTERED_NAME, value: sellerData?.registered_name, visible: true },
+        { label: 'Company Name', name: SellerKeys.NAME, value: sellerData?.name, visible: true },
+        { label: 'Contact Person Name', name: SellerKeys.HEAD_NAME, value: sellerData?.head_name, visible: true },
+        { label: 'Vendor Code', name: SellerKeys.CODE, value: sellerData?.code, visible: true },
+        { label: 'Email Address', name: SellerKeys.HEAD_EMAIL, value: sellerData?.head_email, visible: true },
+        { label: 'Phone Number', name: SellerKeys.HEAD_CONTACT, value: sellerData?.head_contact, visible: true },
+        { label: 'Fashion Style', name: SellerKeys.SEGMENT, value: sellerData?.segment, visible: true },
     ]
 
     const BusinessDetailsDetail = [
-        { label: 'PAN Number', name: 'pan_number', value: sellerData?.pan_number, visible: true },
-        { label: 'TAN Number', name: 'tan_number', value: sellerData?.tan_number, visible: true },
-        { label: 'CIN', name: 'cin', value: sellerData?.cin, visible: true },
-        { label: 'Sole Proprietorship Type', name: 'sp_type', value: sellerData?.sp_type, visible: true },
-        { label: 'Pf Declaration', name: 'pf_declaration', value: sellerData?.pf_declaration, visible: true },
+        { label: 'PAN Number', name: SellerKeys.PAN_NUMBER, value: sellerData?.pan_number, visible: true },
+        { label: 'TAN Number', name: SellerKeys.TAN_NUMBER, value: sellerData?.tan_number, visible: true },
+        { label: 'CIN', name: SellerKeys.CIN, value: sellerData?.cin, visible: true },
+        { label: 'Sole Proprietorship Type', name: SellerKeys.SP_TYPE, value: sellerData?.sp_type, visible: true },
+        { label: 'Pf Declaration', name: SellerKeys.PF_DECLARATION, value: sellerData?.pf_declaration, visible: true },
     ]
 
     const PocDetailsDetail = [
-        { label: 'Vendor POC', name: 'poc', value: sellerData?.poc, visible: true },
-        { label: 'Vendor POC Email', name: 'poc_email', value: sellerData?.poc_email, visible: true },
-        { label: 'Vendor POC Contact', name: 'contact_number', value: sellerData?.contact_number, visible: true },
+        { label: 'Vendor POC', name: SellerKeys.POC, value: sellerData?.poc, visible: true },
+        { label: 'Vendor POC Email', name: SellerKeys.POC_EMAIL, value: sellerData?.poc_email, visible: true },
+        { label: 'Vendor POC Contact', name: SellerKeys.CONTACT_NUMBER, value: sellerData?.contact_number, visible: true },
         {
             label: 'Vendor POC Alternate Number',
-            name: 'alternate_contact_number',
+            name: SellerKeys.ALTERNATE_CONTACT_NUMBER,
             value: sellerData?.alternate_contact_number,
             visible: true,
         },
-        { label: 'Finance Contact Name', name: 'finance_name', value: sellerData?.finance_name, visible: true },
-        { label: 'Finance Contact Number', name: 'finance_contact_number', value: sellerData?.finance_contact_number, visible: true },
-        { label: 'Finance Email', name: 'finance_email', value: sellerData?.finance_email, visible: true },
+        { label: 'Finance Contact Name', name: SellerKeys.FINANCE_NAME, value: sellerData?.finance_name, visible: true },
+        {
+            label: 'Finance Contact Number',
+            name: SellerKeys.FINANCE_CONTACT_NUMBER,
+            value: sellerData?.finance_contact_number,
+            visible: true,
+        },
+        { label: 'Finance Email', name: SellerKeys.FINANCE_EMAIL, value: sellerData?.finance_email, visible: true },
     ]
 
     const SellerBankDetail = [
-        { label: 'Bank Name', name: 'bank_name', value: sellerData?.bank_name, visible: true },
-        { label: 'Branch Name', name: 'branch_name', value: sellerData?.branch_name, visible: true },
-        { label: 'Account Holder Name', name: 'account_holder_name', value: sellerData?.account_holder_name, visible: true },
+        { label: 'Bank Name', name: SellerKeys.BANK_NAME, value: sellerData?.bank_name, visible: true },
+        { label: 'Branch Name', name: SellerKeys.BRANCH_NAME, value: sellerData?.branch_name, visible: true },
+        { label: 'Account Holder Name', name: SellerKeys.ACCOUNT_HOLDER_NAME, value: sellerData?.account_holder_name, visible: true },
         { label: 'Account Number', name: 'account_number', value: sellerData?.account_number, visible: true },
-        { label: 'IFSC Code', name: 'ifsc', value: sellerData?.ifsc, visible: true },
-        { label: 'Account Type', name: 'account_type', value: sellerData?.account_type, visible: true },
+        { label: 'IFSC Code', name: SellerKeys.IFSC, value: sellerData?.ifsc, visible: true },
+        { label: 'Account Type', name: SellerKeys.ACCOUNT_TYPE, value: sellerData?.account_type, visible: true },
     ]
 
-    // const SellerWarehouseDetail = [
-    //     { label: 'Warehouse Name', name: 'warehouse_name', value: sellerData?.warehouse_name, visible: true },
-    //     { label: 'GSTIN', name: 'gstin', value: sellerData?.gstin, visible: true },
-    //     { label: 'WareHouse Active', name: 'is_active', value: sellerData?.is_active, visible: true },
-    // ]
-
     const SellerMsMeDetail = [
-        { label: 'MsMe Category', name: 'msme_category', value: sellerData?.msme_category, visible: true },
-        { label: 'Is Registered Under MsMe', name: 'is_msme', value: sellerData?.is_msme, visible: true },
+        { label: 'MsMe Category', name: SellerKeys.MSME_CATEGORY, value: sellerData?.msme_category, visible: true },
+        { label: 'Is Registered Under MsMe', name: SellerKeys.IS_MSME, value: sellerData?.is_msme, visible: true },
     ]
 
     const SellerCommercialsDetail = [
-        { label: 'Revenue Share Percent', name: 'revenue_share', value: sellerData?.revenue_share, visible: true },
+        { label: 'Revenue Share Percent', name: SellerKeys.REVENUE_SHARE, value: sellerData?.revenue_share, visible: true },
         {
             label: 'Handling Charges Per Sku',
-            name: 'handling_charges_per_order',
+            name: SellerKeys.HANDLING_CHARGES_PER_ORDER,
             value: sellerData?.handling_charges_per_order,
             visible: true,
         },
         {
             label: 'Warehouse Charge Per Sku',
-            name: 'warehouse_charge_per_sku',
+            name: SellerKeys.WAREHOUSE_CHARGE_PER_SKU,
             value: sellerData?.warehouse_charge_per_sku,
             visible: true,
         },
-        { label: 'Damages Per Sku', name: 'damages_per_sku', value: sellerData?.damages_per_sku, visible: true },
-        { label: 'Removal Fee Per Sku', name: 'removal_fee_per_sku', value: sellerData?.removal_fee_per_sku, visible: true },
-        { label: 'Nature of Business', name: 'business_nature', value: sellerData?.business_nature, visible: true },
-        { label: 'Settlement Days', name: 'settlement_days', value: sellerData?.settlement_days, visible: true },
-        { label: 'Approved Payment Terms', name: 'approved_payment_term', value: sellerData?.approved_payment_term, visible: true },
+        { label: 'Damages Per Sku', name: SellerKeys.DAMAGES_PER_SKU, value: sellerData?.damages_per_sku, visible: true },
+        { label: 'Removal Fee Per Sku', name: SellerKeys.REMOVAL_FEE_PER_SKU, value: sellerData?.removal_fee_per_sku, visible: true },
+        { label: 'Nature of Business', name: SellerKeys.BUSINESS_NATURE, value: sellerData?.business_nature, visible: true },
+        { label: 'Settlement Days', name: SellerKeys.SETTLEMENT_DAYS, value: sellerData?.settlement_days, visible: true },
+        {
+            label: 'Approved Payment Terms',
+            name: SellerKeys.APPROVED_PAYMENT_TERM,
+            value: sellerData?.approved_payment_term,
+            visible: true,
+        },
     ]
 
     const SellerInternalDetail = [
-        { label: 'Slikk POC -Category Name', name: 'int_poc_name', value: sellerData?.int_poc_name, visible: true },
-        { label: 'Slikk POC -Category Email', name: 'int_poc_email', value: sellerData?.int_poc_email, visible: true },
-        { label: 'Slikk POC -Category Number', name: 'int_poc_contact', value: sellerData?.int_poc_contact, visible: true },
+        { label: 'Slikk POC -Category Name', name: SellerKeys.INT_POC_NAME, value: sellerData?.int_poc_name, visible: true },
+        { label: 'Slikk POC -Category Email', name: SellerKeys.INT_POC_EMAIL, value: sellerData?.int_poc_email, visible: true },
+        { label: 'Slikk POC -Category Number', name: SellerKeys.INT_POC_CONTACT, value: sellerData?.int_poc_contact, visible: true },
     ]
 
     const SellerDeclarationDetail = [
-        { label: 'Authorized Person', name: 'authorized_person', value: sellerData?.authorized_person, visible: true },
-        { label: 'Declaration Statement', name: 'declaration_statement', value: sellerData?.declaration_statement, visible: true },
+        { label: 'Authorized Person', name: SellerKeys.AUTHORIZED_PERSON, value: sellerData?.authorized_person, visible: true },
+        { label: 'Declaration Statement', name: SellerKeys.DECLARATION_STATEMENT, value: sellerData?.declaration_statement, visible: true },
     ]
 
     const sections = [
@@ -101,49 +107,49 @@ export const SellerDetailCommon = ({ seller: sellerData }: any) => {
             label: 'PAN Card',
             file: sellerData?.pan_copy,
             key: 'pan',
-            name: 'pan_copy',
+            name: SellerKeys.PAN_COPY,
             fieldName: 'panCopyFile',
         },
         {
             label: 'Tan Card',
             file: sellerData?.tan_copy,
             key: 'tan',
-            name: 'tan_copy',
+            name: SellerKeys.TAN_COPY,
             fieldName: 'tanCopyFile',
         },
         {
             label: 'PF Declaration Doc',
             file: sellerData?.pf_declaration_doc,
             key: 'pf',
-            name: 'pf_declaration_doc',
+            name: SellerKeys.PF_DECLARATION_DOC,
             fieldName: 'pd_doc_file',
         },
         {
             label: 'Trade Mark Certificate',
             file: sellerData?.trade_mark_certificate,
             key: 'trade',
-            name: 'trade_mark_certificate',
+            name: SellerKeys.TRADE_MARK_CERTIFICATE,
             fieldName: 'trade_mark_file',
         },
         {
             label: 'GST Certificate',
             file: sellerData?.gst_certificate,
             key: 'gst',
-            name: 'gst_certificate',
+            name: SellerKeys.GST_CERTIFICATE,
             fieldName: 'gstCertificateFile',
         },
         {
             label: 'Cancelled Cheque',
             file: sellerData?.cancelled_cheque,
             key: 'cheque',
-            name: 'cancelled_cheque',
+            name: SellerKeys.CANCELLED_CHEQUE,
             fieldName: 'cancelledChequeFile',
         },
         {
             label: 'MSME Certificate',
             file: sellerData?.msme_certificate,
             key: 'msme',
-            name: 'msme_certificate',
+            name: SellerKeys.MSME_CERTIFICATE,
             fieldName: 'msmeCertificateFile',
         },
     ]
