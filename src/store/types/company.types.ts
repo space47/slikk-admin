d1c15235ba48fdd4d76170cc9ecb09823c2b14f0
Debasishe <debashish@slikk.club>
2026-02-25 00:34:42 +0530

    added company types

--- src/store/types/company.types.ts
+++ src/store/types/company.types.ts
@@ -1,5 +1,29 @@
 import { createAction } from '@reduxjs/toolkit'
 
+export type BUSINESS_NATURE = {
+    bill_to: string
+    code: string
+    company_legal_name: string
+    company_name: string
+    gstin: string
+    registered_address: string
+    ship_to: string
+}
+export type WAREHOUSE_DETAILS = {
+    company: number
+    create_date: string
+    gst_certificate: string
+    gstin: string
+    id: number
+    is_active: boolean
+    poc_contact_number: string
+    poc_email: string
+    poc_name: string
+    update_date: string
+    warehouse_address: string
+    warehouse_name: string
+}
+
 export type SINGLE_COMPANY_DATA = {
     id: number
     registered_name?: string
@@ -21,6 +45,8 @@ export type SINGLE_COMPANY_DATA = {
     update_date?: string
     code?: string
     commercial_approval_doc?: string
+    business_nature_company: BUSINESS_NATURE[]
+    gst_details: WAREHOUSE_DETAILS[]
 }
 
 export type USER_PROFILE_DATA = {
