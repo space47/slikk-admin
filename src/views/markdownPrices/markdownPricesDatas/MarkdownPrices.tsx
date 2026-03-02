b4d2c4b9737841b5398d76e0c22654c3e4a15c02
Debasishe <debashish@slikk.club>
2026-03-03 01:16:39 +0530

    markdown price

--- src/views/markdownPrices/markdownPricesDatas/MarkdownPrices.tsx
+++ src/views/markdownPrices/markdownPricesDatas/MarkdownPrices.tsx
@@ -1,45 +1,33 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { markdownPriceTypes } from '../markdownCommon'
 import axioisInstance from '@/utils/intercepter/globalInterceptorSetup'
-import { FaEdit } from 'react-icons/fa'
+import { FaEdit, FaPlus } from 'react-icons/fa'
 import moment from 'moment'
 import EasyTable from '@/common/EasyTable'
 import PageCommon from '@/common/PageCommon'
 import { Button } from '@/components/ui'
 import { useNavigate } from 'react-router-dom'
 import DialogConfirm from '@/common/DialogConfirm'
-import { notification } from 'antd'
+import { notification, Spin } from 'antd'
 import { RxUpdate } from 'react-icons/rx'
+import { useFetchApi } from '@/commonHooks/useFetchApi'
+import { AxiosError } from 'axios'
+import { errorMessage } from '@/utils/responseMessages'
 
 const MarkdownPrices = () => {
-    const [markdownPricesData, setMarkdownPricesData] = useState<markdownPriceTypes>([])
     const [page, setPage] = useState<number>(1)
     const [pageSize, setPageSize] = useState<number>(10)
     const [globalFilter, setGlobalFilter] = useState<string | null>('')
-    const [totalPages, setTotalPages] = useState<number>(0)
     const [showSync, setShowSync] = useState(false)
     const navigate = useNavigate()
 
-    const fetchMarkdownPrices = async () => {
-        try {
-            let searchFilter = ''
-            if (globalFilter) {
-                searchFilter = `&name=${globalFilter}`
-            }
-
-            const response = await axioisInstance.get(`/product/offer/pricing?p=${page}&ps=${pageSize}${searchFilter}`)
-            const data = response?.data?.data
-            setMarkdownPricesData(data?.results)
-            setTotalPages(data?.count)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    useEffect(() => {
-        fetchMarkdownPrices()
+    const query = useMemo(() => {
+        const searchFilter = globalFilter ? `&name=${globalFilter}` : ''
+        return `/product/offer/pricing?p=${page}&ps=${pageSize}${searchFilter}`
     }, [page, pageSize, globalFilter])
+
+    const { data: markdownPricesData, totalData, loading } = useFetchApi<markdownPriceTypes>({ url: query, initialData: [] })
 
     const columns = useMemo(
         () => [
@@ -120,34 +108,25 @@ const MarkdownPrices = () => {
     }
 
     const handleFacebookSync = async () => {
-        notification.info({
-            message: 'SYNC IN PROCESS',
-        })
-        const body = {
-            task_name: 'update_product_pricing_data',
-        }
+        notification.info({ message: 'SYNC IN PROCESS' })
+        const body = { task_name: 'update_product_pricing_data' }
         setShowSync(false)
         try {
             const response = await axioisInstance.post(`/backend/task/process`, body)
-            notification.success({
-                message: response?.data?.message || 'SYNCED',
-            })
-        } catch (error: any) {
-            console.error(error)
-            notification.error({
-                message: error.response?.data?.message || 'FAILED TO SYNC',
-            })
+            notification.success({ message: response?.data?.message || 'SYNCED' })
+        } catch (error) {
+            if (error instanceof AxiosError) errorMessage(error)
         }
     }
 
     return (
-        <div>
+        <Spin spinning={loading}>
             <div className="flex flex-col gap-4">
                 <div className="flex xl:justify-between xl:flex-row md:flex-row md:justify-between flex-col gap-2">
                     <div className="xl:order-none order-2 ">
                         <input
                             type="search"
-                            placeholder="Enter offer codes"
+                            placeholder="Search by Name"
                             className="w-full border border-gray-300 rounded p-2"
                             value={globalFilter as string}
                             onChange={(e) => setGlobalFilter(e.target?.value)}
@@ -155,23 +134,19 @@ const MarkdownPrices = () => {
                     </div>
                     <div>
                         <div className="flex gap-2 ">
-                            <button
-                                className=" px-4 py-2  xl:flex-row flex flex-row items-center gap-2  hover:bg-green-600 rounded-lg text-white bg-green-500"
-                                onClick={() => setShowSync(true)}
-                            >
-                                <span className="font-bold">Sync</span> <RxUpdate className="text-xl" />
-                            </button>
-                            <div>
-                                <Button onClick={hanldeAddMarkdownPrices} variant="new">
-                                    Add New
-                                </Button>
-                            </div>
+                            <Button variant="accept" size="sm" icon={<RxUpdate />} onClick={() => setShowSync(true)}>
+                                <span className="font-bold">Sync</span>
+                            </Button>
+
+                            <Button variant="new" size="sm" icon={<FaPlus />} onClick={hanldeAddMarkdownPrices}>
+                                Add New
+                            </Button>
                         </div>
                     </div>
                 </div>
                 <EasyTable overflow mainData={markdownPricesData} columns={columns} page={page} pageSize={pageSize} />
                 {!globalFilter && (
-                    <PageCommon page={page} setPage={setPage} pageSize={pageSize} totalData={totalPages} setPageSize={setPageSize} />
+                    <PageCommon page={page} setPage={setPage} pageSize={pageSize} totalData={totalData} setPageSize={setPageSize} />
                 )}
             </div>
             {showSync && (
@@ -183,7 +158,7 @@ const MarkdownPrices = () => {
                     headingName="Update Pricing"
                 />
             )}
-        </div>
+        </Spin>
     )
 }
 
