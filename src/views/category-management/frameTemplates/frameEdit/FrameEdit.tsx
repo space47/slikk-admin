03b5c0a99f95d03e420801d5f99b07c2dd0c33a7
saksham-slikk <saksham@slikk.club>
2026-04-07 01:51:19 +0530

    fix funciton duplication

--- src/views/category-management/frameTemplates/frameEdit/FrameEdit.tsx
+++ src/views/category-management/frameTemplates/frameEdit/FrameEdit.tsx
@@ -43,16 +43,6 @@ const rgbaTupleToAlpha = (rgba: number[]): number => {
     return rgba[3]
 }
 
-const hexToRgbaTuple = (hex: string, alpha: number) => {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-    return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16), alpha] : [0, 0, 0, alpha]
-}
-
-const rgbaTupleToAlpha = (rgba: number[]): number => {
-    if (!rgba || rgba.length < 4) return 255
-    return rgba[3]
-}
-
 interface AutoPreviewProps {
     values: TemplateConfig
     setPreviewImage: React.Dispatch<React.SetStateAction<string | null>>
