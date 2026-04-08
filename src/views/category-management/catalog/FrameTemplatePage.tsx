e510a06edc751e682cd704acc8619b449c465d49
saksham-slikk <saksham@slikk.club>
2026-04-08 13:47:24 +0530

    create hex-rgb utility function

--- src/views/category-management/catalog/FrameTemplatePage.tsx
+++ src/views/category-management/catalog/FrameTemplatePage.tsx
@@ -11,16 +11,7 @@ import { generatePreviewCanvas, TemplateConfig } from './generatePreviewCanvas'
 import { useNavigate } from 'react-router-dom'
 import debounce from 'lodash/debounce'
 import { FrameFields } from './frameCommon'
-
-const hexToRgbTuple = (hex: string) => {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-    return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : [0, 0, 0]
-}
-
-const hexToRgbaTuple = (hex: string, alpha: number) => {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-    return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16), alpha] : [0, 0, 0, alpha]
-}
+import { hexToRgbTuple, hexToRgbaTuple } from '@/utils/colors'
 
 interface AutoPreviewProps {
     values: TemplateConfig
